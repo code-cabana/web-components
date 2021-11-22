aa94eb719fe9d0a946ce0906d67a76d55620c440
Loki <sneyed@gmail.com>
2021-11-22 22:01:26 +1000

    both wrapping seamless working

--- packages/web-components/src/components/carousel/carousel.component.js
+++ packages/web-components/src/components/carousel/carousel.component.js
@@ -16,36 +16,6 @@ function Carousel({
     ? useSwipe({ onSwipeStart, onSwiping, onSwipeEnd })
     : useRef();
 
-  function isAtEdge() {
-    const atStart = position <= 0;
-    const atEnd = position >= trackWidth - viewportWidth;
-    const atEdge = atStart || atEnd;
-    return { atEdge, atStart, atEnd };
-  }
-
-  function onSwipeStart() {
-    trackRef.current.style.transition = "none";
-  }
-
-  function onSwiping({ directions }) {
-    const { left, right } = directions;
-    const delta = left - right;
-
-    const { atEdge: needsToWrap, atEnd } = isAtEdge();
-    if (needsToWrap)
-      setBasePosition(atEnd ? viewportWidth : trackWidth - viewportWidth * 2);
-    else setSwipeDelta(delta);
-    return needsToWrap;
-  }
-
-  function onSwipeEnd() {
-    trackRef.current.style.transition = "";
-    setBasePosition(
-      clamp(basePosition + swipeDelta, 0, trackWidth - viewportWidth)
-    );
-    setSwipeDelta(0);
-  }
-
   const rawItems = [...Array(10)].map((_, id) => ({ id }));
   const items = loop
     ? [
@@ -59,24 +29,23 @@ function Carousel({
   const [viewportWidth, setViewportWidth] = useState();
   const [itemWidth, setItemWidth] = useState();
   const [activeItem, setActiveItem] = useState(0);
-  const [basePosition, setBasePosition] = useStateCb(0);
-  const [swipeDelta, setSwipeDelta] = useState(0);
+  const [basePosition, setBasePosition] = useStateCb(0); // Position excluding swipe data
+  const [swipeDelta, setSwipeDelta] = useState(0); // Pixel length of the current swipe
 
   const trackWidth = items.length * itemWidth;
-  const position = clamp(
-    basePosition + swipeDelta,
-    0,
-    trackWidth - viewportWidth
-  );
+  const endEdgePos = trackWidth - viewportWidth;
+  const position = clamp(basePosition + swipeDelta, 0, endEdgePos);
 
-  function getViewportWidth() {
-    return viewportRef?.current?.clientWidth;
-  }
+  useEffect(updateDimensions, [viewportRef]);
+  useEffect(goToLoopStart, [itemWidth]);
+
+  // Initialization
+  // ---
 
   // Updates the viewport and item widths
   function updateDimensions() {
     if (!viewportRef?.current) return;
-    const viewportWidth = getViewportWidth();
+    const viewportWidth = viewportRef?.current?.clientWidth;
     const itemWidth = viewportWidth / itemsPerViewport;
     setViewportWidth(viewportWidth);
     setItemWidth(itemWidth);
@@ -89,69 +58,111 @@ function Carousel({
     setActiveItem(loopStart);
   }
 
-  useEffect(updateDimensions, [viewportRef]);
-  useEffect(goToLoopStart, [itemWidth]);
-
-  // Arrow key navigation
-  function onKeyDown(event) {
-    if (event.repeat) return;
-    const { atStart, atEnd } = isAtEdge();
-    const goingLeft = event.key === "ArrowLeft";
-    const goingRight = event.key === "ArrowRight";
-    const needsToWrap = (atStart && goingLeft) || (atEnd && goingRight);
-
-    if (needsToWrap) {
-      // Determine distance to edge of the loop
-      const { x: computedPosition } = getMatrixTranslateValues(
-        getComputedStyle(trackRef?.current).getPropertyValue("transform")
-      );
-      const distanceToEdge = atStart
-        ? Math.abs(computedPosition)
-        : trackWidth - (Math.abs(computedPosition) + viewportWidth);
-
-      // Snap to our current position, but on the other side of the loop
-      const immediatePosition = atEnd
-        ? viewportWidth
-        : trackWidth - viewportWidth * 2;
-      const immediateItem = atEnd
-        ? itemsPerViewport
-        : items.length - itemsPerViewport * 2;
-      const offsetPosition = atEnd
-        ? immediatePosition - distanceToEdge
-        : immediatePosition + distanceToEdge;
-      setActiveItem(immediateItem);
-      setBasePosition(offsetPosition, () => {
-        trackRef.current.style.transition = "none"; // Disable transition
-        requestAnimationFrame(() => {
-          trackRef.current.style.transition = ""; //Re-enable transition
-          // Determine the next position to move towards
-          const moveToPosition = atEnd
-            ? immediatePosition + itemWidth
-            : immediatePosition - itemWidth;
-          const moveToItem = atEnd ? immediateItem + 1 : immediateItem - 1;
-          setBasePosition(moveToPosition);
-          setActiveItem(moveToItem);
-        });
-      });
-    } else {
-      if (goingRight) adjustActiveItem(1);
-      else if (goingLeft) adjustActiveItem(-1);
-    }
-  }
-
-  function adjustActiveItem(count) {
-    const newItem = clamp(
-      activeItem + count,
-      0,
-      items.length - itemsPerViewport
-    );
-    const newPos = getItemPosition(newItem);
-    setActiveItem(newItem);
-    setBasePosition(newPos);
-  }
+  // Getters
+  // ---
 
   function getItemPosition(index) {
     return index * itemWidth;
+  }
+
+  // Swiping
+  // ---
+
+  function onSwipeStart() {
+    trackRef.current.style.transition = "none";
+  }
+
+  function onSwiping({ directions }) {
+    const { left, right } = directions;
+    const delta = left - right;
+
+    const { atEdge: needsToWrap, atEnd } = isAtEdge();
+    if (needsToWrap) {
+      setBasePosition(atEnd ? viewportWidth : endEdgePos - viewportWidth);
+      setSwipeDelta(0);
+    } else setSwipeDelta(delta);
+    return needsToWrap;
+  }
+
+  function onSwipeEnd() {
+    trackRef.current.style.transition = "";
+    setBasePosition(clamp(basePosition + swipeDelta, 0, endEdgePos));
+    setSwipeDelta(0);
+  }
+
+  // Navigation
+  // ---
+
+  function isAtEdge() {
+    const atStart = position <= 0;
+    const atEnd = position >= endEdgePos;
+    const atEdge = atStart || atEnd;
+    return { atEdge, atStart, atEnd };
+  }
+
+  function wrapCurrentPosition() {
+    if (!trackRef.current) return;
+    const { atEdge, atEnd } = isAtEdge();
+    if (!atEdge) return;
+
+    const { x: computedPosition } = getMatrixTranslateValues(
+      getComputedStyle(trackRef?.current).getPropertyValue("transform")
+    );
+    const distanceToEdge = atEnd
+      ? trackWidth - (Math.abs(computedPosition) + viewportWidth)
+      : Math.abs(computedPosition); // Determine distance to edge of the loop
+
+    // Snap to our current position, but on the other side of the loop
+    const snapPosition = atEnd ? viewportWidth : endEdgePos - viewportWidth;
+    const immediateItem = atEnd
+      ? itemsPerViewport
+      : items.length - itemsPerViewport * 2;
+    const offsetPosition = atEnd
+      ? snapPosition - distanceToEdge
+      : snapPosition + distanceToEdge;
+
+    trackRef.current.style.transition = "none"; // Disable transition
+
+    setActiveItem(immediateItem); // Immediately set activeItem to the same item on the other side of loop
+    setBasePosition(offsetPosition, () => {
+      // Immediately set to the offset position and then wait for next animation frame
+      requestAnimationFrame(() => {
+        trackRef.current.style.transition = ""; // Re-enable transition
+
+        const moveToPosition = atEnd
+          ? snapPosition + itemWidth
+          : snapPosition - itemWidth; // Determine the next position to move towards
+        const moveToItem = atEnd ? immediateItem + 1 : immediateItem - 1;
+        setBasePosition(moveToPosition); // Go to the next item's position
+        setActiveItem(moveToItem);
+      });
+    });
+  }
+
+  function adjustActiveItem(count) {
+    const { atStart, atEnd } = isAtEdge();
+    const goingLeft = Math.sign(count) === -1;
+    const goingRight = Math.sign(count) === 1;
+    const needsToWrap = (atStart && goingLeft) || (atEnd && goingRight);
+
+    if (needsToWrap) {
+      wrapCurrentPosition();
+    } else {
+      const newItem = clamp(
+        activeItem + count,
+        0,
+        items.length - itemsPerViewport
+      );
+      const newPos = getItemPosition(newItem);
+      setActiveItem(newItem);
+      setBasePosition(newPos);
+    }
+  }
+
+  function onKeyDown(event) {
+    if (event.repeat) return;
+    if (event.key === "ArrowRight") adjustActiveItem(1);
+    else if (event.key === "ArrowLeft") adjustActiveItem(-1);
   }
 
   return (
