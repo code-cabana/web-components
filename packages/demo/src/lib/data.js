3a5159f72600ba572097fc3fe1986200dda2b32b
Loki <sneyed@gmail.com>
2021-12-26 19:36:02 +1000

    make published vs unpublished via blacklist clear in demo site

--- packages/demo/src/lib/data.js
+++ packages/demo/src/lib/data.js
@@ -1,7 +1,10 @@
 import glob from "tiny-glob";
+import { blacklist } from "../../../web-components/src/config/components";
 
 export async function getComponentList(pathPrefix = "..") {
   return (
     await glob(`${pathPrefix}/web-components/src/components/**/*.component.js`)
-  ).map((path) => path.match(/.*[\\\/](.*?).component.js/)[1]);
+  )
+    .map((path) => path.match(/.*[\\\/](.*?).component.js/)[1])
+    .filter((component) => !blacklist.includes(component));
 }
--- packages/demo/src/pages/index.js
+++ packages/demo/src/pages/index.js
@@ -1,12 +1,23 @@
 import MainLayout from "../layouts/main";
 import { getComponentList } from "../lib/data";
+import { blacklist } from "../../../web-components/src/config/components";
 
-export default function Home({ components }) {
+export default function Home({ components, blacklist }) {
   return (
     <MainLayout>
       <h1>Code Cabana Web Components</h1>
+      <h2>Published</h2>
       <ul>
         {components.map((component, index) => (
+          <li key={index}>
+            <a href={`/components/${component}`}>{component}</a>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Unpublished</h2>
+      <ul>
+        {blacklist.map((component, index) => (
           <li key={index}>
             <a href={`/components/${component}`}>{component}</a>
           </li>
@@ -19,6 +30,6 @@ export default function Home({ components }) {
 export async function getStaticProps() {
   const components = await getComponentList();
   return {
-    props: { components },
+    props: { components, blacklist },
   };
 }
