677c531028f17516b1cc3ca19aef13d14d8d410d
Loki <sneyed@gmail.com>
2021-12-30 21:54:22 +1000

    remove unused hooks

--- packages/web-components/src/lib/hooks/index.js
+++ packages/web-components/src/lib/hooks/index.js
@@ -1,5 +1,3 @@
 export { default as useEventListener } from "./useEventListener";
 export { default as useSwipe } from "./useSwipe";
-export { default as useOnClickOutside } from "./useOnClickOutside";
-export { default as useAnimationFrame } from "./useAnimationFrame";
 export { default as useStateCb } from "./useStateCb";
